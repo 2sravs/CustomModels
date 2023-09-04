b58668a59f6d7588e371826c31759e2f3d1a821d
2Sravs <sravani.chekuri1243#gmail.com>
2023-09-04 18:10:18 +0530

    firstcommit

---
+++ Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomModel2.Models
+{
+    public class StudentModel
+    {
+       
+            public string Name { get; set; }
+            public string WhichClass { get; set; }
+            public string Address { get; set; }
+            public int TotalMarks { get; set; }
+
+
+        
+    }
+}
